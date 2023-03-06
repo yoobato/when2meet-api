b7bddd4c0a2e60bf30594fb2bd7ea9b7857fb120
yoobato <yoobato@gmail.com>
2023-03-07 00:55:29 +0900

    Add validation advie

---
+++ src/main/java/kr/or/kftc/when2meet/validator/ValidationAdvisor.java
@@ -0,0 +1,33 @@
+package kr.or.kftc.when2meet.validator;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ValidationAdvisor extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("status", status.value());
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
+        body.put("errors", errors);
+
+        return ResponseEntity.status(status).headers(headers).body(body);
+    }
+}
