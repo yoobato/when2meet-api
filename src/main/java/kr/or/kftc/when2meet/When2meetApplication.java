b24045b2aeb0e563148da092ffede40a4c35408d
yoobato <yoobato@gmail.com>
2023-03-06 23:56:48 +0900

    Initial commit

---
+++ src/main/java/kr/or/kftc/when2meet/When2meetApplication.java
@@ -0,0 +1,13 @@
+package kr.or.kftc.when2meet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class When2meetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(When2meetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/or/kftc/when2meet/When2meetApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.kftc.when2meet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class When2meetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
